b5890d7d7c8ac93bdd765fe66af44e70d8e8fe13
Juan Angela Alma <juanlah77@gmail.com>
2023-07-20 23:57:32 +0700

    add handler response

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -10,7 +10,8 @@ use Illuminate\Support\Facades\Validator;
 
 class KaryawanController extends Controller
 {
-    public function getAllKaryawan() {
+    public function getAllKaryawan()
+    {
         try {
             $karyawan = Karyawan::all();
             return ApiResponseService::success($karyawan, "Sukses mengambil data karyawan");
@@ -19,7 +20,8 @@ class KaryawanController extends Controller
         }
     }
 
-    public function createKaryawan(Request $request) {
+    public function createKaryawan(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'nama' => 'required|string|max:50',
             'alamat' => 'required|string|max:100',
@@ -27,24 +29,29 @@ class KaryawanController extends Controller
             'tanggal_bergabung' => 'required|date'
         ]);
 
-        if($validator->fails()) {
+        if ($validator->fails()) {
             return ApiResponseService::error($validator->errors(), 400);
         }
 
-        $newNomorInduk = Karyawan::generateNomorInduk();
+        try {
+            $newNomorInduk = Karyawan::generateNomorInduk();
 
-        $newKaryawan = Karyawan::create([
-            'nomor_induk' => $newNomorInduk,
-            'nama' => $request->nama,
-            'alamat' => $request->alamat,
-            'tanggal_lahir' => $request->tanggal_lahir,
-            'tanggal_bergabung' => $request->tanggal_bergabung,
-        ]);
+            $newKaryawan = Karyawan::create([
+                'nomor_induk' => $newNomorInduk,
+                'nama' => $request->nama,
+                'alamat' => $request->alamat,
+                'tanggal_lahir' => $request->tanggal_lahir,
+                'tanggal_bergabung' => $request->tanggal_bergabung,
+            ]);
 
-        return ApiResponseService::success($newKaryawan, "Sukses menambah karyawan", 201);
+            return ApiResponseService::success($newKaryawan, "Sukses menambah karyawan", 201);
+        } catch (Exception $e) {
+            return ApiResponseService::error($e->getMessage(), 500);
+        }
     }
 
-    public function updateKaryawan(Request $request, $nomorInduk) {
+    public function updateKaryawan(Request $request, $nomorInduk)
+    {
         $validator = Validator::make($request->all(), [
             'nama' => 'required|string|max:50',
             'alamat' => 'required|string|max:100',
@@ -52,31 +59,45 @@ class KaryawanController extends Controller
             'tanggal_bergabung' => 'required|date'
         ]);
 
-        if($validator->fails()) {
+        if ($validator->fails()) {
             return ApiResponseService::error($validator->errors(), 400);
         }
 
         $karyawan = Karyawan::where('nomor_induk', $nomorInduk)->first();
 
-        if(!$karyawan) {
+        if (!$karyawan) {
             return ApiResponseService::error("Karyawan dengan nomor induk $nomorInduk tidak ditemukan", 404);
         }
 
-        $karyawan->nama = $request->nama;
-        $karyawan->tanggal_lahir = $request->tanggal_lahir;
-        $karyawan->tanggal_bergabung = $request->tanggal_bergabung;
+        try {
+            $karyawan->nama = $request->nama;
+            $karyawan->tanggal_lahir = $request->tanggal_lahir;
+            $karyawan->tanggal_bergabung = $request->tanggal_bergabung;
 
-        return ApiResponseService::success($karyawan, "Sukses mengupdate karyawan", 201);
+            return ApiResponseService::success($karyawan, "Sukses mengupdate karyawan", 201);
+        } catch (Exception $e) {
+            return ApiResponseService::error($e->getMessage(), 500);
+        }
     }
 
-    public function deleteKaryawan($nomorInduk) {
-        $karyawan = Karyawan::where('nomor_induk', $nomorInduk);
-        $karyawan->delete();
-        return ApiResponseService::success(null, null, 204);
+    public function deleteKaryawan($nomorInduk)
+    {
+        try {
+            $karyawan = Karyawan::where('nomor_induk', $nomorInduk);
+            $karyawan->delete();
+            return ApiResponseService::success(null, null, 204);
+        } catch (Exception $e) {
+            return ApiResponseService::error($e->getMessage(), 500);
+        }
     }
 
-    public function getKaryawanTerbaru() {
-        $karyawanTerbaru = Karyawan::orderBy('tanggal_bergabung', 'DESC')->take(3)->get();
-        return ApiResponseService::success($karyawanTerbaru, "Sukses mendapatkan data karyawan terbaru");
+    public function getKaryawanTerbaru()
+    {
+        try {
+            $karyawanTerbaru = Karyawan::orderBy('tanggal_bergabung', 'DESC')->take(3)->get();
+            return ApiResponseService::success($karyawanTerbaru, "Sukses mendapatkan data karyawan terbaru");
+        } catch (Exception $e) {
+            return ApiResponseService::error($e->getMessage(), 500);
+        }
     }
 }
