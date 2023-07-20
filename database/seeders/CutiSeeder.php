5d4ee54280933a47f575db098c0b32b4a5dacf04
Juan Angela Alma <juanlah77@gmail.com>
2023-07-20 18:18:22 +0700

    created seeder

---
+++ database/seeders/CutiSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cuti;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CutiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'nomor_induk' => 'IP06001',
+                'tanggal_cuti' => '2020/08/02',
+                'lama_cuti' => '2',
+                'keterangan' => 'Acara Keluarga'
+            ],
+            [
+                'nomor_induk' => 'IP06001',
+                'tanggal_cuti' => '2020/08/18',
+                'lama_cuti' => '2',
+                'keterangan' => 'Anak Sakit'
+            ],
+            [
+                'nomor_induk' => 'IP06006',
+                'tanggal_cuti' => '2020/08/19',
+                'lama_cuti' => '1',
+                'keterangan' => 'Nenek Sakit'
+            ],
+            [
+                'nomor_induk' => 'IP06007',
+                'tanggal_cuti' => '2020/08/23',
+                'lama_cuti' => '1',
+                'keterangan' => 'Sakit'
+            ],
+            [
+                'nomor_induk' => 'IP06004',
+                'tanggal_cuti' => '2020/08/29',
+                'lama_cuti' => '5',
+                'keterangan' => 'Menikah'
+            ],
+            [
+                'nomor_induk' => 'IP06003',
+                'tanggal_cuti' => '2020/08/30',
+                'lama_cuti' => '2',
+                'keterangan' => 'Acara Keluarga'
+            ],
+        ];
+
+        Cuti::insert($data);
+    }
+}
